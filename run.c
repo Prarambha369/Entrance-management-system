e77217f1ddd823a60f6309f194618adce6065763
mrbashyal <prarambhabashyal@gmail.com>
2025-07-04 00:23:56 +0545

    Add function prototypes for student record management in run.c

--- run.c
+++ run.c
@@ -7,3 +7,13 @@
 #include<conio.h>
 #include<ctype.h>
 #define Student struct Stud
+
+void add(FILE * fp); //to add to list
+void exam(FILE *fp);//for entrance examination
+void show_record();
+void modify(FILE * fp);//to modify a record
+void displayList(FILE * fp);//display whole list
+void searchRecord(FILE *fp);//find a particular record
+void printChar(char ch,int n);//printing a character ch n times
+void printHead();//printing head line for each screen
+void sort(FILE *fp);//sorting
